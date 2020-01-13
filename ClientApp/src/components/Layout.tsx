d59766223de026446e325ca1f7954c15bf769974
Pierce Lauring <pclauring@gmail.com>
2020-01-13 17:05:51 -0500

    Now it really keeps the footer in place

--- ClientApp/src/components/Layout.tsx
+++ ClientApp/src/components/Layout.tsx
@@ -5,9 +5,9 @@ import LogoFooter from "./LogoFooter";
 
 export default (props: { children?: React.ReactNode }) => (
   <React.Fragment>
-    <NavMenu />
-    {/* this to keep the footer in place */}
+    {/* this is to keep the footer in place */}
     <div style={{ position: "relative", minHeight: "100vh" }}>
+      <NavMenu />
       <div style={{ paddingBottom: "16rem" }}>
         <Container>{props.children}</Container>
       </div>
