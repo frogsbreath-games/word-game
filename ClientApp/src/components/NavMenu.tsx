ba6cd77dcd9a91691fc48efb6989b5a07c4e1598
Benjamin Chodos <bdchodos@gmail.com>
2020-01-19 16:02:24 -0500

    Removed Counter and WeatherForecast template leftovers

--- ClientApp/src/components/NavMenu.tsx
+++ ClientApp/src/components/NavMenu.tsx
@@ -47,16 +47,6 @@ export default class NavMenu extends React.PureComponent<
                     Game
                   </NavLink>
                 </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">
-                    Counter
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">
-                    Fetch data
-                  </NavLink>
-                </NavItem>
               </ul>
             </Collapse>
           </Container>
