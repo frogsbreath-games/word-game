ad973dbc8c744f2bf6e51cf913084837329d0750
Benjamin Chodos <bdchodos@gmail.com>
2020-02-26 21:49:11 -0500

    added 1.0 names

--- Application/Resources/CharacterList.cs
+++ Application/Resources/CharacterList.cs
@@ -11,17 +11,16 @@ namespace WordGame.API.Application.Resources
 	{
 		public static Character[] Characters = new[]
 		{
-			//TODO - Names
-			new Character("Cultist", CharacterType.Cultist, 0, Team.Red),
-			new Character("Cultist", CharacterType.Cultist, 1, Team.Blue),
-			new Character("Researcher 1", CharacterType.Researcher, 2, Team.Red),
-			new Character("Researcher 1", CharacterType.Researcher, 3, Team.Blue),
-			new Character("Researcher 2", CharacterType.Researcher, 4, Team.Red),
-			new Character("Researcher 2", CharacterType.Researcher, 5, Team.Blue),
-			new Character("Researcher 3", CharacterType.Researcher, 6, Team.Red),
-			new Character("Researcher 3", CharacterType.Researcher, 7, Team.Blue),
-			new Character("Researcher 4", CharacterType.Researcher, 8, Team.Red),
-			new Character("Researcher 4", CharacterType.Researcher, 9, Team.Blue),
+			new Character("Azami D'aathess", CharacterType.Cultist, 0, Team.Red),
+			new Character("Z'arri Zhaorru Zuibberh", CharacterType.Cultist, 1, Team.Blue),
+			new Character("Dmitry Koshkin", CharacterType.Researcher, 2, Team.Red),
+			new Character("Father Alfred Moore", CharacterType.Researcher, 3, Team.Blue),
+			new Character("Osip Belinsky", CharacterType.Researcher, 4, Team.Red),
+			new Character("Inspector Raymond Bernard", CharacterType.Researcher, 5, Team.Blue),
+			new Character("Tatyana Ulanov", CharacterType.Researcher, 6, Team.Red),
+			new Character("Professor Clayton Womack", CharacterType.Researcher, 7, Team.Blue),
+			new Character("Komandarm Igor Yeltsin", CharacterType.Researcher, 8, Team.Red),
+			new Character("Dr Eloise Winthrop", CharacterType.Researcher, 9, Team.Blue),
 		};
 	}
 }
